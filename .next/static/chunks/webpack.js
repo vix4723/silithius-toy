773d37d18403ebdfcb6d48a427f2aaa9394d2697
Vix <63135033+vix4723@users.noreply.github.com>
2024-06-27 17:06:43 -0500

    updating

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6c01b13f11a14231"; }
+/******/ 		__webpack_require__.h = function() { return "13f99cefc1b060fb"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
